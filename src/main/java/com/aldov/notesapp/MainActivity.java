57659ddf3099b42d93455d26558659f9e0bc2d11
ALDO <aldocode@gmail.com>
2013-07-05 19:43:04 +0530

    Insert layout

---
+++ src/main/java/com/aldov/notesapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.aldov.notesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = (Button)findViewById(R.id.insertHomeButton);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,InsertActivity.class);
+                startActivity(i);
+
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
